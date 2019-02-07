9bc92f7cf5588780071a77a134ab500cec16f914
eslam <eslamkhalefa19@gmail.com>
2019-02-07 18:59:05 +0200

    search bar component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import SearchBar from "../containers/SearchBar";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
---
+++ src/containers/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  state = {
+    term: ""
+  };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
